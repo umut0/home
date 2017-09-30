f465b520b4ae258ba21d0be2bad95c460f33bb9c
umut0 <tas@we-build.city>
2017-09-30 16:29:25 -0700

    Updates

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { trigger, state, animate, transition, style } from '@angular/animations';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'ut-home',
@@ -18,6 +19,30 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
           opacity: 0
         }))
       )
+    ]),
+    trigger('slideInOut', [
+      state('*', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(100%)'}),
+        animate('2s cubic-bezier(0.215, 0.610, 0.355, 1.000)')
+      ]),
+      transition('* => void',
+        animate('2s cubic-bezier(0.215, 0.610, 0.355, 1.000)', style({
+          transform: 'translateX(-100%)'
+        }))
+      )
+    ]),
+    trigger('fadeInOut', [
+      state('*', style({opacity: 1})),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate('0.4s 0.9s linear')
+      ]),
+      transition('* => void',
+        animate('0.3s linear', style({
+          opacity: 0
+        }))
+      )
     ])
   ],
 })
@@ -27,6 +52,30 @@ export class HomeComponent implements OnInit {
   constructor(public dataService: DataService) { }
 
   ngOnInit() {
+      let that = this;
+
+      this.dataService.getData().subscribe( data => {
+          console.log(data.startpage);
+          data.startpage.products.forEach(function(item){
+              Observable.interval(5000).subscribe(x => {
+                  that.increaseCount(item);
+              });
+          });
+      });
+      // console.log(this.dataService.data);
+    // this.dataService.data.startpage.products.forEach(function(item){
+    //     Observable.interval(5000).subscribe(x => {
+    //         console.log(x);
+    //         console.log(item);
+    //     });
+    // });
+  }
+
+  increaseCount(item){
+      item.active++;
+      if (item.active >= item.content.length) {
+          item.active = 0;
+      }
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class DataService {
 
     public data: Object;
+    // public obsData: Observable<Object> = new Observable<Object>();
+    public subjectData = new Subject<Object>();
 
   constructor(private http: Http) { 
       http.get("./assets/data/data.json").subscribe( data => {
           this.data = data.json();
           console.log(this.data);
+          this.subjectData.next(this.data);
+          // this.obsData.next(data.json());
       })
+  }
 
+  getData(): Observable<any> {
+      return this.subjectData.asObservable();
   }
 
 }
