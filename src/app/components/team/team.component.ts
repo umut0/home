8b0ce4647b393a2b890f35eb1ce71c43a2975e3a
umut0 <tas@we-build.city>
2017-10-02 18:20:29 -0700

    anim

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { trigger, state, animate, transition, style } from '@angular/animations';
+
 
 @Component({
   selector: 'ut-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  styleUrls: ['./team.component.scss'],
+  animations : [
+  trigger('fadeInOut', [
+      state('*', style({opacity: 1})),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate('0.4s 0.9s linear')
+      ]),
+      transition('* => void',
+        animate('0.3s linear', style({
+          opacity: 0
+        }))
+      )
+    ])
+  ]
 })
 export class TeamComponent implements OnInit {
 
